818ea44cb00f796fbab20f615829a352e96ce682
volirina <74723644+volirina@users.noreply.github.com>
2023-05-11 14:36:39 -0400

    assets 3

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,13 +7,20 @@ using RPG.Core;
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
-    {
-        [SerializeField] float weaponRange = 2f;
+    {        
         [SerializeField] float timeBetweenAttacks = 1f;
-        [SerializeField] float weaponDamage = 10;
+        [SerializeField] Transform handTransform = null;
+        [SerializeField] Weapon defaultWeapon = null;
+
         Health target;
         float timeSinceLastAttack = Mathf.Infinity;
+        Weapon currentWeapon = null;
 
+        private void Start()
+        {
+            EquipWeapon(defaultWeapon);
+        }
+        
         private void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
@@ -30,6 +37,12 @@ namespace RPG.Combat
                 GetComponent<Mover>().Cancel();
                 AttackVehaviour();
             }
+        }
+        public void EquipWeapon(Weapon weapon)
+        {
+            currentWeapon = weapon;
+            Animator animator = GetComponent<Animator>();
+            weapon.Spawn(handTransform, animator);
         }
 
         private void AttackVehaviour()
@@ -53,12 +66,12 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) return;
-            target.TakeDamage(weaponDamage);
+            target.TakeDamage(currentWeapon.GetDamage());
         }
 
         private bool GetIsInRange()
         {
-            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetRange();
         }
 
         public bool CanAttack(GameObject combatTarget)
